3595efe59f1bd38bab96f5a3f0c50aef6f4555d7
Julius Henke <mail@juliushenke.com>
2021-04-20 22:42:48 +0200

    Initial commit, Setup backend Spring Boot Kotlin, Add READMEs

---
+++ backend/src/test/kotlin/com/torusage/TorUsageApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.torusage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TorUsageApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
