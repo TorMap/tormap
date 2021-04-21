b403b7ebf38b63d5337e0c9e8d11edcc45202b0f
Julius Henke <mail@juliushenke.com>
2021-04-21 14:34:51 +0200

    Update README.md and scripts

--- scripts/backend.sh
+++ scripts/backend.sh
@@ -4,7 +4,7 @@ pkill -f 'gradle'
 cd ../backend || exit
 if [ -f './gradlew' ]; then
   ./gradlew > /dev/null 2>&1
-  ./gradlew run --args='--global' > /dev/null 2>&1
+  ./gradlew bootRun > /dev/null 2>&1
 else
   echo 'ERROR: No Gradle wrapper found. Please make sure /backend/gradlew is available.'
 fi
