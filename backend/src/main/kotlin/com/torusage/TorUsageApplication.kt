42e8a1607bcea7ec5fa3df37c38bcead988d8d57
Julius Henke <mail@juliushenke.com>
2021-05-18 16:16:14 +0200

    Remove debug code

--- backend/src/main/kotlin/com/torusage/TorUsageApplication.kt
+++ backend/src/main/kotlin/com/torusage/TorUsageApplication.kt
@@ -1,32 +1,20 @@
 package com.torusage
 
 import com.torusage.common.logger
-import com.torusage.database.repository.RelaySummaryRepository
-import com.torusage.database.entity.RelaySummary
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 import java.time.ZonedDateTime
-import javax.persistence.EntityManager
 
 
 @SpringBootApplication
 @EnableScheduling
-class TorUsageApplication(
-    val relaySummaryRepository: RelaySummaryRepository,
-    val entityManager: EntityManager
-) : ApplicationRunner {
+class TorUsageApplication() : ApplicationRunner {
 
     override fun run(args: ApplicationArguments) {
         logger().info("TorUsage backend started successfully, running in timezone: " + ZonedDateTime.now().zone)
-        val testList = listOf("a","b")
-        val test = RelaySummary("test","test", a = testList, true)
-        relaySummaryRepository.save(test)
-        logger().info(relaySummaryRepository.findByN("test")!!.f)
-        var a = relaySummaryRepository.findByN("test")!!.a
-        var testrepo = relaySummaryRepository.findAll()
     }
 
 }
