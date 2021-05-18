0b6d2a69c88545cee5ffe2e382ed0a9e0d4e59d9
Julius Henke <mail@juliushenke.com>
2021-05-18 11:43:48 +0200

    Add KDoc and package for code documentation

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,12 +1,18 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    // Spring
     id("org.springframework.boot") version "2.4.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+
+    // Kotlin extensions
     kotlin("jvm") version "1.4.32"
     kotlin("plugin.spring") version "1.4.32"
     kotlin("plugin.allopen") version "1.4.32"
     kotlin("plugin.jpa") version "1.4.32"
+
+    // Code documentation
+    id("org.jetbrains.dokka") version "1.4.32"
 }
 
 group = "com.torusage"
@@ -29,11 +35,14 @@ dependencies {
     // Serialization & Deserialization
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
-    // Database Stuff
+    // Database
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     runtimeOnly("com.h2database:h2")
-    runtimeOnly("org.postgresql:postgresql")
+
+    // Code documentation
+    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.4.32")
+
     // Testing
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
