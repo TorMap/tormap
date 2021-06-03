29c9f24b6f2ffeb85ffb42686cd27cedaf51c71c
Tim Kilb <timkilb@outlook.de>
2021-06-03 11:33:51 +0200

    leaflet integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import "./leaflet.css"
+
+
+const style={
+    height: "800%"
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <MapContainer center={[0,0]} zoom={1} scrollWheelZoom={false} style={style}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
     </div>
   );
 }
