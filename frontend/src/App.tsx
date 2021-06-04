fde04dfa5c302d41266f5482a895242237d5e055
Tim Kilb <timkilb@outlook.de>
2021-06-04 18:55:04 +0200

    wraped map over to whole screen, added json with testdata for development, added markers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,26 @@
 import React from 'react';
 import './App.css';
-import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
-import "./leaflet.css"
-
-
-const style={
-    height: "800%"
-}
+import {MapContainer, Marker, TileLayer} from "react-leaflet";
+import * as test_data from './Example-data-details.json';
 
 function App() {
   return (
-    <div className="App">
-            <MapContainer center={[0,0]} zoom={1} scrollWheelZoom={false} style={style}>
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-            </MapContainer>
-    </div>
+        <MapContainer center={[0,0]} zoom={3} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {
+                test_data.relays.map(relay => {
+                    if (!(isNaN(relay.latitude))) {
+                        return  (<Marker
+                                    key={relay.nickname}
+                                    position={[relay.latitude, relay.longitude]}
+                                />)
+                    } else {return -1};
+                })
+            }
+        </MapContainer>
   );
 }
 
