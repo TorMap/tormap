7bc14eb625b81961777f283542fe291b5f9b6d24
Tim Kilb <timkilb@outlook.de>
2021-06-08 13:47:13 +0200

    restructured part of the frontend

---
+++ frontend/src/utils/types.tsx
@@ -0,0 +1,10 @@
+type basicRelay = {
+    fingerprint: string;
+    as: string;
+    first_seen: number;
+    last_see: number;
+    latitude: number;
+    longitude: number;
+};
+
+export default basicRelay;
