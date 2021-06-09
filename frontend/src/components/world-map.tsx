5290c3f360cb569a1ff917711b91129e9700897b
Julius Henke <mail@juliushenke.com>
2021-06-09 02:39:36 +0200

    Use CircleMarker instead of Marker, Improve map options, Cleanup unused files

--- frontend/src/components/world-map.tsx
+++ frontend/src/components/world-map.tsx
@@ -1,4 +1,5 @@
-import {MapContainer, Marker, TileLayer} from "react-leaflet";
+
+import {CircleMarker, MapContainer, TileLayer} from "react-leaflet";
 import React, {useEffect, useState} from "react";
 import {Relay} from "../types/relay";
 
@@ -8,23 +9,37 @@ export const WorldMap = () => {
     useEffect(() => {
         console.log("Fetching relays")
         fetch('http://localhost:8080/node/relays')
-        .then(res => res.json())
-        .then((data) => {
-            setRelays(data)
-            console.log("Fetched relays")
-        })
-        .catch(console.log)
+            .then(response => response.json())
+            .then((relays: Relay[]) => {
+                setRelays(relays)
+                console.log("Fetched relays")
+            })
+            .catch(console.log)
     }, []);
 
     return (
-        <MapContainer center={[15,0]} zoom={3} scrollWheelZoom={false}>
+        <MapContainer
+            center={[15, 0]}
+            minZoom={2}
+            zoom={3}
+            scrollWheelZoom={true}
+            wheelPxPerZoomLevel={200}
+            preferCanvas={true}
+        >
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
                 subdomains="abcd"
                 maxZoom={19}
                 url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+                noWrap={true}
             />
-            {relays.map(relay => <Marker key={relay.fingerprint} position={[relay.latitude, relay.longitude]}/>)}
+            {relays.map(relay =>
+                <CircleMarker
+                    key={relay.fingerprint}
+                    center={[relay.latitude, relay.longitude]}
+                    radius={1}
+                />
+            )}
         </MapContainer>
     );
 };
