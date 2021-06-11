a0fc5aaeb03bc19acc4259766ce3a10a04828aa6
Tim Kilb <timkilb@outlook.de>
2021-06-11 10:00:20 +0200

    added missing styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,29 @@
 import React, {useState} from 'react';
 import {WorldMap} from "./components/world-map/world-map";
 import ReactSlidingPane from "react-sliding-pane";
-import {Button} from "@material-ui/core";
+import {Button, makeStyles} from "@material-ui/core";
 import "@material-ui/styles"
 import "./app.css"
 
 function App() {
     const [showOptionPane, setShowOptionPane] = useState(false)
  //ToDo: Button and optionPane styling
+    const css= makeStyles({
+        root:{
+            backgroundColor: "white",
+            position: "fixed",
+            bottom: "10px",
+            left: "50%",
+        }
+    });
+    const style = css();
+
+
   return (
       <div>
           <WorldMap/>
-          <Button onClick={() => setShowOptionPane(!showOptionPane)} >toggle overlay</Button>
-          <ReactSlidingPane isOpen={showOptionPane} onRequestClose={() => setShowOptionPane(false)} from={"bottom"} title={"Optionen"}>
+          <Button className={style.root} onClick={() => setShowOptionPane(!showOptionPane)}>toggle overlay</Button>
+          <ReactSlidingPane width={"100%"} isOpen={showOptionPane} onRequestClose={() => setShowOptionPane(false)} from={"bottom"} title={"Optionen"}>
               <h1>optionen</h1>
           </ReactSlidingPane>
 
