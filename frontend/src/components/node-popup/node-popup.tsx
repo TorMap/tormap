867753c6a2a9a4270a0ea7e5b0709147d44db31f
Julius Henke <mail@juliushenke.com>
2021-06-15 21:55:12 +0200

    Process relay information for node popup dialog

--- frontend/src/components/node-popup/node-popup.tsx
+++ frontend/src/components/node-popup/node-popup.tsx
@@ -6,21 +6,54 @@ import {
     Typography
 } from "@material-ui/core";
 import CloseIcon from "@material-ui/icons/Close";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./node-popup.scss"
 import {Relay} from "../../types/relay";
+import {apiBaseUrl} from "../../util/constants";
 
 interface Props{
+    /**
+     * Whether the node popup will be shown
+     */
     showNodePopup: boolean,
+    /**
+     * Method to set dialog visibility
+     */
     setShowNodePopup: () => void,
-    relay?: Relay,
+    /**
+     * Id of the relay to be displayed
+     */
+    relayId?: number,
 }
 
 export const NodePopup: React.FunctionComponent<Props> = ({
     showNodePopup,
     setShowNodePopup,
-    relay,
+    relayId,
 }) => {
+    const [relay, setRelay] = useState<Relay>()
+    const [relayInfos, setRelayInfos] = useState<RelayInfo[]>()
+
+    useEffect(() => {
+        fetch(apiBaseUrl + "/node/relay/" + relayId)
+            .then(response => response.json())
+            .then((relay: Relay) => {
+                setRelay(relay)
+                setRelayInfos([
+                        {name: "Fingerprint", value: relay.fingerprint},
+                        {name: "AS family", value: relay.as_name},
+                        {name: "Contact", value: relay.contact},
+                        {name: "First seen", value: relay.first_seen},
+                        {name: "Last seen", value: relay.last_seen},
+                        {name: "Flags", value: relay.flags?.join(", ")},
+                        {name: "Observed bandwidth", value: relay.observed_bandwidth ? (relay.observed_bandwidth / 1000000).toFixed(2) + " MB/s" : undefined},
+                        {name: "Consensus weight", value: relay.consensus_weight},
+                        {name: "Platform", value: relay.platform},
+                ])
+            })
+            .catch(console.log)
+    }, [relayId])
+
     return(
         <Dialog
             open={showNodePopup}
@@ -39,10 +72,15 @@ export const NodePopup: React.FunctionComponent<Props> = ({
                 dividers
                 className={"dialogfield"}
             >
-                <Typography>
-                    {JSON.stringify(relay)}
-                </Typography>
+                {relayInfos?.map((relayInfo) =>
+                    relayInfo.value ? <p><b>{relayInfo.name}</b>: {relayInfo.value}</p> : undefined
+                )}
             </DialogContent>
         </Dialog>
     );
+}
+
+interface RelayInfo {
+    name: string
+    value: string | number | undefined
 }
