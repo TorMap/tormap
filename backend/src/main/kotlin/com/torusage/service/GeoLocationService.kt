64df1ff7d7714fd14cb89edcd5c6e33ce7d7e0c0
Julius Henke <mail@juliushenke.com>
2021-07-01 18:10:28 +0200

    Refactor code
    Former-commit-id: 80c27b9221c12a73ee539a44347f5e8da1285e62

--- backend/src/main/kotlin/com/torusage/service/GeoLocationService.kt
+++ backend/src/main/kotlin/com/torusage/service/GeoLocationService.kt
@@ -3,6 +3,7 @@ package com.torusage.service
 import com.maxmind.db.CHMCache
 import com.maxmind.geoip2.DatabaseReader
 import com.maxmind.geoip2.model.CityResponse
+import com.maxmind.geoip2.record.Location
 import com.torusage.logger
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
@@ -17,13 +18,22 @@ class GeoLocationService(
     @Value("\${geo.location.database.resource.file}")
     private val databaseResourceFile: String,
 ) {
-    val logger = logger()
-    val databaseReader: DatabaseReader = DatabaseReader.Builder(
+    private val logger = logger()
+    private val databaseReader: DatabaseReader = DatabaseReader.Builder(
         javaClass.getResourceAsStream(databaseResourceFile)
     ).withCache(CHMCache()).build()
 
     fun getCityLocationForIpAddress(ipAddress: String): CityResponse? = try {
         databaseReader.city(InetAddress.getByName(ipAddress))
+    } catch (exception: Exception) {
+        logger.warn("Location lookup failed for IP address $ipAddress! " + exception.message)
+        null
+    }
+
+    fun getLocationForIpAddress(ipAddress: String): Location? = try {
+        val location = databaseReader.city(InetAddress.getByName(ipAddress)).location
+        if (location.latitude == null || location.longitude == null) throw Exception("Latitude and longitude were not found!")
+        location
     } catch (exception: Exception) {
         logger.warn("Location lookup failed for IP address $ipAddress! " + exception.message)
         null
