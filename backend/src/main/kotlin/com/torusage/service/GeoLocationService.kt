c06aa4994333e05a9986d57d0d94a40900a9c2fd
Julius Henke <mail@juliushenke.com>
2021-07-01 20:28:46 +0200

    Refactor code
    Former-commit-id: bb4374221b64829f1d04dabf93fd3f4a232c1224

--- backend/src/main/kotlin/com/torusage/service/GeoLocationService.kt
+++ backend/src/main/kotlin/com/torusage/service/GeoLocationService.kt
@@ -3,7 +3,6 @@ package com.torusage.service
 import com.ip2location.IP2Location
 import com.maxmind.db.CHMCache
 import com.maxmind.geoip2.DatabaseReader
-import com.maxmind.geoip2.model.CityResponse
 import com.torusage.logger
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
@@ -32,23 +31,27 @@ class GeoLocationService(
 
     fun getLocationForIpAddress(ipAddress: String): GeoLocation? = try {
         val location = maxmindDatabaseReader.city(InetAddress.getByName(ipAddress)).location
-        if (location.latitude == null || location.longitude == null) throw Exception("Latitude and longitude missing!")
+        if (location.latitude == null || location.longitude == null) throw GeoException()
         GeoLocation(location.longitude, location.latitude)
     } catch (exception: Exception) {
         logger.warn("Maxmind location lookup failed for IP address $ipAddress! " + exception.message)
         try {
             val location = ip2locationDatabaseReader.ipQuery(ipAddress)
             if (location.status != "OK") throw Exception(location.status)
-            else if (location.latitude == null || location.longitude == null) throw Exception("Latitude and longitude missing!")
+            else if (location.latitude == null || location.longitude == null) throw GeoException()
             GeoLocation(location.latitude!!.toDouble(), location.longitude!!.toDouble())
         } catch (exception: Exception) {
             logger.warn("IP2Location location lookup failed for IP address $ipAddress! " + exception.message)
             null
         }
     }
+
+
 }
 
 data class GeoLocation(
     var longitude: Double,
     var latitude: Double,
 )
+
+class GeoException: Exception("Latitude and longitude missing!")
