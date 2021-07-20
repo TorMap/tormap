655afc7239d33424d463ade70aabde4f3b86a3cc
Julius Henke <mail@juliushenke.com>
2021-07-20 16:36:23 +0200

    getGeoRelaysByDay endpoint
    Former-commit-id: f8e83301333f55f787000572240492d4743f1592

--- backend/src/main/kotlin/com/torusage/database/repository/archive/ArchiveGeoRelayRepositoryImpl.kt
+++ backend/src/main/kotlin/com/torusage/database/repository/archive/ArchiveGeoRelayRepositoryImpl.kt
@@ -1,10 +1,13 @@
 package com.torusage.database.repository.archive
 
+import org.springframework.data.jpa.repository.Query
+import java.time.LocalDate
+
 
 /**
  * Repository to interact with DB
  */
 interface ArchiveGeoRelayRepositoryImpl : ArchiveGeoRelayRepository {
-//    @Query("SELECT DISTINCT id.seenInMonth FROM ArchiveGeoRelay ORDER BY id.seenInMonth")
-//    fun findDistinctMonths(): List<String>
+    @Query("SELECT DISTINCT day FROM ArchiveGeoRelay ORDER BY day")
+    fun findDistinctDays(): List<LocalDate>
 }
