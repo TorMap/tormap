a966a9336ac02fe9156d3a49cd9d43aafbe629a1
Tim Kilb <timkilb@outlook.de>
2021-07-21 19:53:12 +0200

    wip Map Legend and added more filters
    Former-commit-id: f8dc042e988cbb01891a29824c8f4043b777c62a

---
+++ frontend/src/components/legend/map-legend.tsx
@@ -0,0 +1,29 @@
+import {FunctionComponent} from "react";
+import {Card, CardContent, Typography} from "@material-ui/core";
+import {Statistics} from "../../types/variousTypes";
+import "./map-legend.scss"
+
+interface Props {
+    statistics: Statistics
+}
+
+export const MapStats: FunctionComponent<Props> = ({statistics}) => {
+
+    return (
+        <div className={"map-stats"}>
+            <Card variant={"outlined"}>
+                <CardContent>
+                    <Typography>
+                        Exit relays: {statistics.exit}
+                    </Typography>
+                    <Typography>
+                        Guard relays: {statistics.guard}
+                    </Typography>
+                    <Typography>
+                        Other relays: {statistics.default}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
