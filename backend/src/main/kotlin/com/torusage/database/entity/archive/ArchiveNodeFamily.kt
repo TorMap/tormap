e325402c7b6370c00996250587aa8c321240239b
Julius Henke <mail@juliushenke.com>
2021-07-22 16:18:33 +0200

    Reduce ArchiveNodeDetails.kt to month precision
    Former-commit-id: 1bf0ad5d37003dbf86f6107c028cc7fa5d1132ea

---
+++ backend/src/main/kotlin/com/torusage/database/entity/archive/ArchiveNodeFamily.kt
@@ -0,0 +1,27 @@
+package com.torusage.database.entity.archive
+
+import com.torusage.jointToCommaSeparated
+import org.torproject.descriptor.NetworkStatusEntry
+import java.util.*
+import javax.persistence.*
+
+/**
+ * This entity is used to store relevant information about a [NetworkStatusEntry]
+ */
+@Suppress("unused")
+@Entity
+@Table(
+    indexes = [
+        Index(columnList = "month, fingerprints", unique = true),
+    ]
+)
+class ArchiveNodeFamily(
+    sortedFingerprints: SortedSet<String>,
+    var month: String,
+
+    @Id
+    @GeneratedValue
+    val id: Long? = null,
+) {
+    val fingerprints: String = sortedFingerprints.jointToCommaSeparated()!!
+}
