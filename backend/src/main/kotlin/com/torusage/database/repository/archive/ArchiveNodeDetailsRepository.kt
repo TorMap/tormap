e72ba1de59f3b56aa7a5f5919f0cc9c53e4a75cd
Julius Henke <mail@juliushenke.com>
2021-07-22 18:31:15 +0200

    Add node family analyzing
    Former-commit-id: 3358ed28dae2804ff25943ec60284df1ba4960cc

--- backend/src/main/kotlin/com/torusage/database/repository/archive/ArchiveNodeDetailsRepository.kt
+++ backend/src/main/kotlin/com/torusage/database/repository/archive/ArchiveNodeDetailsRepository.kt
@@ -5,4 +5,6 @@ import org.springframework.data.repository.CrudRepository
 
 interface ArchiveNodeDetailsRepository : CrudRepository<ArchiveNodeDetails, Long> {
     fun getByMonthAndFingerprint(month: String, fingerprint: String): ArchiveNodeDetails?
+    fun getAllByMonthAndNickname(month: String, nickname: String): List<ArchiveNodeDetails>
+    fun getAllByMonthAndFamilyEntriesIsNotNull(month: String): List<ArchiveNodeDetails>
 }
