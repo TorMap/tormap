e531d8839ac314ce6fe7870d087ea0b1b3adee93
Julius Henke <mail@juliushenke.com>
2021-07-25 14:11:16 +0200

    Add country code to geo relay response
    Former-commit-id: ea06966ada4b187b9738956b65812bcc5076db0e

--- frontend/src/types/geo-relay.ts
+++ frontend/src/types/geo-relay.ts
@@ -5,6 +5,7 @@ import {RelayFlag} from "./relay";
 export interface GeoRelayView {
     lat: number
     long: number
+    country: string
     flags?: RelayFlag[]
     detailsId: string
     familyId: string
