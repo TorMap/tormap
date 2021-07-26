2bc60a907d4f87e33604339dbf884c85801c46dd
Tim Kilb <timkilb@outlook.de>
2021-07-26 16:39:57 +0200

    added settings to only render selected country/family
    Former-commit-id: 14211a56d993998b130e5b35d5fdb7b7e0cfe028

--- frontend/src/components/map-stats.tsx
+++ frontend/src/components/map-stats.tsx
@@ -1,5 +1,15 @@
 import {FunctionComponent} from "react";
-import {Card, CardContent, makeStyles, Typography} from "@material-ui/core";
+import {
+    Card,
+    CardContent,
+    makeStyles,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableRow,
+    Typography
+} from "@material-ui/core";
 import {Settings, Statistics} from "../types/variousTypes";
 
 const useStyle = makeStyles(() => ({
@@ -7,6 +17,7 @@ const useStyle = makeStyles(() => ({
         position: "fixed",
         left: "10px",
         bottom: "100px",
+        width: "250px",
     }
 }))
 
@@ -17,25 +28,59 @@ interface Props {
 
 export const MapStats: FunctionComponent<Props> = ({settings, statistics}) => {
     const classes = useStyle()
+    const table = true
+
+    const rows = (sett: Settings, stat: Statistics) => {
+        let row: Array<string[]> = []
+        if (stat.exit) row.push(["Exit relays", stat.exit.toString()])
+        if (stat.guard) row.push(["Guard relays", stat.guard.toString()])
+        if (stat.default) row.push(["Other Relays", stat.default.toString()])
+        if (stat.exit || stat.guard || stat.default) row.push(["Total amount of Relays", (stat.exit + stat.guard + stat.default).toString()])
+        if (settings.sortFamily) row.push(["Selectet Family", settings.selectedFamily == undefined ? "none" : settings.selectedFamily.toString()])
+        if (settings.sortCountry) row.push(["Selectet Country", settings.selectedCountry == undefined ? "none" : settings.selectedCountry])
+        return row
+    }
+
     return (
-            <Card variant={"outlined"} className={classes.root}>
-                <CardContent>
-                    <Typography>
-                        Exit relays: {statistics.exit}
-                    </Typography>
-                    <Typography>
-                        Guard relays: {statistics.guard}
-                    </Typography>
-                    <Typography>
-                        Other relays: {statistics.default}
-                    </Typography>
-                    <Typography>
-                        maxValueOnSameCoordinate: {statistics.maxValueOnSameCoordinate}
-                    </Typography>
-                    <Typography>
-                        selected Country: {settings.selectedCountry}
-                    </Typography>
-                </CardContent>
-            </Card>
+        <div>
+            {table ? (
+                <TableContainer component={Card} className={classes.root}>
+                    <Table size="small">
+                        <TableBody>
+                            {rows(settings, statistics).map(row => (
+                                <TableRow key={row[0]}>
+                                    <TableCell component="th" scope="row">
+                                        {row[0]}
+                                    </TableCell>
+                                    <TableCell align={"center"}>
+                                        {row[1]}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            ) : (
+                <Card variant={"outlined"} className={classes.root}>
+                    <CardContent>
+                        <Typography>
+                            Exit relays: {statistics.exit}
+                        </Typography>
+                        <Typography>
+                            Guard relays: {statistics.guard}
+                        </Typography>
+                        <Typography>
+                            Other relays: {statistics.default}
+                        </Typography>
+                        <Typography>
+                            maxValueOnSameCoordinate: {statistics.maxValueOnSameCoordinate}
+                        </Typography>
+                        <Typography>
+                            selected Country: {settings.selectedCountry}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            )}
+        </div>
     )
 }
