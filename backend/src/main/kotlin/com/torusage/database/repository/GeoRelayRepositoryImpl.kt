1cf8a87fdffb7bf0abcf8a85f0b46de49919c3f9
Julius Henke <mail@juliushenke.com>
2021-07-29 14:57:54 +0200

    Add config classes
    Former-commit-id: d8715da2c227c5b88cdcfc9cf29be0d322f66f18

--- backend/src/main/kotlin/com/torusage/database/repository/GeoRelayRepositoryImpl.kt
+++ backend/src/main/kotlin/com/torusage/database/repository/GeoRelayRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.torusage.database.repository
 
+import com.torusage.adapter.controller.view.GeoRelayViewTest
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.transaction.annotation.Transactional
@@ -22,4 +23,12 @@ interface GeoRelayRepositoryImpl : GeoRelayRepository {
                 "where exists (SELECT d.id FROM NodeDetails d WHERE function('FORMATDATETIME', g.day, 'yyyy-MM') = d.month AND g.fingerprint = d.fingerprint)"
     )
     fun updateForeignIds(): Int
+
+    @Query(
+        "SELECT new com.torusage.adapter.controller.view.GeoRelayViewTest(g.latitude, g.longitude, g.countryIsoCode, g.flags, n.id, n.familyId) FROM GeoRelay g " +
+                "LEFT JOIN FETCH NodeDetails n " +
+                "ON g.fingerprint = n.fingerprint " +
+                "AND function('FORMATDATETIME', g.day, 'yyyy-MM') = n.month where g.day = :day"
+    )
+    fun findAllUsingDay(day: LocalDate): List<GeoRelayViewTest>
 }
