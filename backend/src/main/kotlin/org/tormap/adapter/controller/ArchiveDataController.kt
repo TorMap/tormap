e1298bcbb4b58ed9fe04acefaafaa5a634484431
Julius Henke <mail@juliushenke.com>
2021-08-17 21:36:46 +0200

    Add /archive/node/identifiers endpoint
    Former-commit-id: 7bf21bc12334222289a9e970ddd7355e544635ec

--- backend/src/main/kotlin/org/tormap/adapter/controller/ArchiveDataController.kt
+++ backend/src/main/kotlin/org/tormap/adapter/controller/ArchiveDataController.kt
@@ -1,21 +1,18 @@
 package org.tormap.adapter.controller
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.tormap.adapter.controller.exception.NodeNotFoundException
 import org.tormap.adapter.controller.view.GeoRelayView
 import org.tormap.database.entity.NodeDetails
 import org.tormap.database.repository.GeoRelayRepositoryImpl
-import org.tormap.database.repository.NodeDetailsRepository
+import org.tormap.database.repository.NodeDetailsRepositoryImpl
 import java.time.LocalDate
 
 @RestController
 @RequestMapping("archive")
 class ArchiveDataController(
     val geoRelayRepository: GeoRelayRepositoryImpl,
-    val nodeDetailsRepository: NodeDetailsRepository,
+    val nodeDetailsRepositoryImpl: NodeDetailsRepositoryImpl,
 ) {
     @GetMapping("/geo/relay/days")
     fun getDaysForGeoRelays() = geoRelayRepository.findDistinctDays()
@@ -26,10 +23,13 @@ class ArchiveDataController(
 
     @GetMapping("/node/details/{id}")
     fun getNodeDetails(@PathVariable id: Long): NodeDetails {
-        val details = nodeDetailsRepository.findById(id)
+        val details = nodeDetailsRepositoryImpl.findById(id)
         return if (details.isPresent) details.get() else throw NodeNotFoundException()
     }
 
     @GetMapping("/node/family/{id}")
-    fun getNodeFamily(@PathVariable id: Long) = nodeDetailsRepository.findAllByFamilyId(id)
+    fun getNodeFamily(@PathVariable id: Long) = nodeDetailsRepositoryImpl.findAllByFamilyId(id)
+
+    @PostMapping("/node/identifiers")
+    fun getNodeIdentifiers(@RequestBody ids: List<Long>) = nodeDetailsRepositoryImpl.findNodeIdentifiers(ids)
 }
--- backend/src/main/kotlin/org/tormap/database/repository/NodeDetailsRepositoryImpl.kt
+++ backend/src/main/kotlin/org/tormap/database/repository/NodeDetailsRepositoryImpl.kt
@@ -1,10 +1,15 @@
 package org.tormap.database.repository
 
 import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.CrudRepository
-import org.tormap.database.entity.NodeDetails
+import org.tormap.adapter.controller.view.NodeIdentifiers
 
 interface NodeDetailsRepositoryImpl : NodeDetailsRepository {
     @Query("SELECT DISTINCT month FROM NodeDetails ORDER BY month")
     fun findDistinctMonths(): Set<String>
+
+    @Query("SELECT new org.tormap.adapter.controller.view.NodeIdentifiers(id, fingerprint, nickname) " +
+            "FROM NodeDetails " +
+            "WHERE id in :ids"
+    )
+    fun findNodeIdentifiers(ids: List<Long>): List<NodeIdentifiers>
 }
