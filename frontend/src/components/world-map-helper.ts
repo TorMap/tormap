27797671332373ace942408057129a9c48525bd4
Tim Kilb <timkilb@outlook.de>
2021-08-17 15:47:56 +0200

    prototype on popup for relay groups
    Former-commit-id: 40ce731cad02299fe6c124bd1805d999dcddf04e

--- frontend/src/components/world-map-helper.ts
+++ frontend/src/components/world-map-helper.ts
@@ -2,8 +2,10 @@
 import {GeoRelayView} from "../types/geo-relay";
 import {RelayFlag} from "../types/relay";
 import {Feature, GeoJsonProperties, GeometryObject} from "geojson";
-import {Layer} from "leaflet";
-import {Settings} from "../types/variousTypes";
+import L, {circleMarker, Layer, LayerGroup, LeafletMouseEvent} from "leaflet";
+import {Settings, Statistics} from "../types/variousTypes";
+import {Colors} from "../util/Config";
+import worldGeoData from "../data/world.geo.json";
 
 export const applyFilter = (relays: GeoRelayView[], settings: Settings): GeoRelayView[] => {
     let filtered: GeoRelayView[] = []
@@ -38,8 +40,7 @@ export const applyFilter = (relays: GeoRelayView[], settings: Settings): GeoRela
 
 //Helper for relaysToLayerGroup, used for adding eventlisteners to the countries
 export const onEachFeature = (
-    feature: Feature<GeometryObject,
-    GeoJsonProperties>,
+    feature: Feature<GeometryObject, GeoJsonProperties>,
     layer: Layer,
     settings: Settings,
     setSettingsCallback: (s: Settings) => void
@@ -152,4 +153,244 @@ export const sortFamCordMap = (
     })
 
     return output
+}
+
+export const getCountryMap = (
+    relays: GeoRelayView[],
+    settings: Settings,
+    setSettingsCallback: (s: Settings) => void
+): Map<string, GeoRelayView[]> => {
+    let countryMap: Map<string, GeoRelayView[]> = new Map<string, GeoRelayView[]>()
+    // true for forcing the calculation to include it in statistics
+    if (settings.sortCountry) {
+        relays.forEach(relay => {
+            if (relay.country !== undefined) {
+                const key: string = relay.country
+                if (countryMap.has(key)) {
+                    let old = countryMap.get(key)!!
+                    old.push(relay)
+                    countryMap.set(key, old)
+                } else {
+                    countryMap.set(key, [relay])
+                }
+            }
+        })
+        if(settings.selectedCountry && !countryMap.has(settings.selectedCountry)){
+            setSettingsCallback({...settings, selectedCountry: undefined})
+        }
+    }
+    return countryMap
+}
+
+export const aggregatedCoordinatesLayer = (
+    latLonMap: Map<string, GeoRelayView[]>,
+    onMarkerClick: (e: LeafletMouseEvent) => void,
+): LayerGroup => {
+    const aggregatedCoordinatesLayer = new LayerGroup()
+    latLonMap.forEach((value, key) => {
+        const coordinates= key.split(",")
+        // skip if a coordinate has less than 4 relays
+        if (value.length < 4) return
+        circleMarker(
+            [+coordinates[0],+coordinates[1]],
+            {
+                radius: value.length / 2,
+                color: "#ffffff",
+                weight: .3,
+                className: key
+            }
+        )
+            .on("click", onMarkerClick)
+            .addTo(aggregatedCoordinatesLayer)
+    })
+    return aggregatedCoordinatesLayer
+}
+
+export const defaultMarkerLayer = (
+    relays: GeoRelayView[],
+    onMarkerClick: (e: LeafletMouseEvent) => void,
+): LayerGroup => {
+    const defaultLayer = new LayerGroup()
+    const exitLayer = new LayerGroup()
+    const guardLayer = new LayerGroup()
+    const defaultMarkerLayer = new LayerGroup([defaultLayer, guardLayer, exitLayer])
+    relays.forEach(relay => {
+        let color = Colors.Default
+        let layer = defaultLayer
+        if (relay.flags?.includes(RelayFlag.Exit)) {
+            color = Colors.Exit
+            layer = exitLayer
+        }
+        else if (relay.flags?.includes(RelayFlag.Guard)) {
+            color = Colors.Guard
+            layer = guardLayer
+        }
+
+        circleMarker(
+            [relay.lat, relay.long],
+            {
+                radius: 1,
+                className: relay.detailsId,
+                color: color,
+                weight: 3,
+            },
+        )
+            .on("click", onMarkerClick)
+            .addTo(layer)
+    })
+    return defaultMarkerLayer
+}
+
+
+export const familyLayer = (
+    familyMap: Map<number, GeoRelayView[]>,
+    settings: Settings,
+    setSettingsCallback: (s: Settings) => void,
+): LayerGroup => {
+    const familyLayer: LayerGroup = new LayerGroup()
+    let index = 0
+    familyMap.forEach(family => {
+        family.forEach((relay,i , family) => {
+            let hue = index * 360 / familyMap.size * (2 / 3)
+            let sat = "90%"
+            let radius = family.length * 10
+
+            if (settings.selectedFamily !== undefined && settings.selectedFamily !== relay.familyId) sat = "30%"
+            if (settings.selectedFamily !== undefined && settings.selectedFamily && settings.selectedFamily !== relay.familyId) sat = "0%"
+
+            const color = `hsl(${hue},${sat},60%)`
+            circleMarker(
+                [relay.lat, relay.long],
+                {color: color,
+                    radius: radius,
+                    fillOpacity: .05,
+                    weight: .1,
+                }
+            )
+                .on("click", () => {
+                    if (relay.familyId === settings.selectedFamily) {
+                        setSettingsCallback({...settings, selectedFamily: undefined})
+                    }else{
+                        setSettingsCallback({...settings, selectedFamily: relay.familyId})
+                    }
+                })
+                .addTo(familyLayer)
+        })
+        index ++
+    })
+    return familyLayer
+}
+
+//todo: scaling anpassen
+export const familyCordLayer = (
+    famCordMap: Map<string, Map<number, GeoRelayView[]>>,
+    settings: Settings,
+    setSettingsCallback: (s: Settings) => void,
+): LayerGroup => {
+    const familyLayer: LayerGroup = new LayerGroup()
+    const sortedFamCordMap: Map<string, famCordArr[]> = sortFamCordMap(famCordMap)
+    sortedFamCordMap.forEach((famMapForLocation ,location) => {
+        const coordinates= location.split(",")
+        const latlng: L.LatLngExpression = [+coordinates[0],+coordinates[1]]
+        famMapForLocation.forEach((famCordArr) => {
+            let hue = famCordArr.familyID % 360
+            let sat = "90%"
+            let radius = famCordArr.relays.length * 6 + famCordArr.padding * 5 + famMapForLocation.length * 0
+
+            if (settings.selectedFamily !== undefined && settings.selectedFamily !== famCordArr.familyID) sat = "30%"
+            if (settings.selectedFamily !== undefined && settings.selectedFamily && settings.selectedFamily !== famCordArr.familyID) sat = "0%"
+
+            const color = `hsl(${hue},${sat},60%)`
+            circleMarker(
+                latlng,
+                {color: color,
+                    radius: radius,
+                    fillOpacity: .25,
+                    weight: .5,
+                }
+            )
+                .on("click", () => {
+                    if (famCordArr.familyID === settings.selectedFamily) {
+                        setSettingsCallback({...settings, selectedFamily: undefined})
+                    }else{
+                        setSettingsCallback({...settings, selectedFamily: famCordArr.familyID})
+                    }
+                })
+                .addTo(familyLayer)
+        })
+    })
+    return familyLayer
+}
+
+export const countryMarkerLayer = (
+    countryMap: Map<string, GeoRelayView[]>,
+    settings: Settings,
+    onMarkerClick: (e: LeafletMouseEvent) => void,
+): LayerGroup => {
+    const countryLayer: LayerGroup = new LayerGroup()
+    let index = 0
+    const geoData = worldGeoData
+    countryMap.forEach((country, key) => {
+        let hue = 0
+        const mapColor9 = (geoData.features.find(feature => feature.properties.iso_a2 === key)?.properties.mapcolor9)
+        if (mapColor9) hue = mapColor9 * 360 / 9
+        else hue = 9 * 360 / 9
+
+        country.forEach((relay,i , country) => {
+            let sat = "90%"
+            let radius = 1
+
+            if (settings.selectedCountry !== undefined && settings.selectedCountry !== relay.country) sat = "30%"
+            if (settings.selectedCountry !== undefined && settings.selectedCountry && settings.selectedCountry !== relay.country) sat = "0%"
+
+            const color = `hsl(${hue},${sat},60%)`
+            circleMarker(
+                [relay.lat, relay.long],
+                {color: color,
+                    radius: radius,
+                    className: relay.detailsId,
+                }
+            )
+                .on("click", onMarkerClick)
+                .addTo(countryLayer)
+        })
+        index ++
+    })
+    return countryLayer
+}
+
+export const calculateStatistics = (
+    relays: GeoRelayView[],
+    countryMap: Map<string, GeoRelayView[]>,
+    familyMap: Map<number, GeoRelayView[]>,
+    settings: Settings,
+): Statistics => {
+    let stats: Statistics = {
+        guard: 0,
+        exit: 0,
+        default: 0,
+    }
+    relays.forEach(relay => {
+        if (relay.flags?.includes(RelayFlag.Exit)) {
+            stats.exit++
+        }else if( relay.flags?.includes(RelayFlag.Guard)){
+            stats.guard++
+        }else{
+            stats.default++
+        }
+    })
+
+    // true for forcing the calculation to include it in statistics
+    if(settings.sortCountry || true){
+        stats = {...stats, countryCount: countryMap.size}
+        if(settings.selectedCountry) {
+            stats = {...stats, countryRelayCount: countryMap.get(settings.selectedCountry)?.length}
+        }
+    }
+    // true for forcing the calculation to include it in statistics
+    if(settings.sortFamily || true){
+        stats = {...stats, familyCount: familyMap.size}
+        if(settings.selectedFamily) stats = {...stats, familyRelayCount: familyMap.get(settings.selectedFamily)?.length}
+    }
+    return stats
 }
--- frontend/src/types/colors.ts
+++
@@ -1,7 +0,0 @@
-export enum Colors{
-    NeonRed= "#FF5733",
-    NeonOrange="#FF5F1F",
-    NeonGelb="#FFFF00",
-    NeonGruen="#00FF00",
-    NeonBlau="#00FFFF",
-}
