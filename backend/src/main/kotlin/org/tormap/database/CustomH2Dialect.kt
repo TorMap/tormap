1cf8a4a154edbd41efec6e7ea3beed71233c619c
Julius Henke <mail@juliushenke.com>
2021-08-23 17:16:50 +0200

    Add autonomous systems to family identifiers
    Former-commit-id: 779c3a17ae9d8b421748b3a1541f71bec0fab8ec

--- backend/src/main/kotlin/org/tormap/database/CustomH2Dialect.kt
+++ backend/src/main/kotlin/org/tormap/database/CustomH2Dialect.kt
@@ -2,14 +2,16 @@ package org.tormap.database
 
 import org.hibernate.dialect.H2Dialect
 import org.hibernate.dialect.function.StandardSQLFunction
+import org.hibernate.type.StandardBasicTypes
 
 /**
  * A custom dialect for the H2 database to advanced register functions
  */
+@Suppress("unused")
 class CustomH2Dialect: H2Dialect() {
 
     init {
-        registerFunction("LISTAGG", StandardSQLFunction("LISTAGG"))
+        registerFunction("LISTAGG", StandardSQLFunction("LISTAGG", StandardBasicTypes.STRING))
     }
 
 }
