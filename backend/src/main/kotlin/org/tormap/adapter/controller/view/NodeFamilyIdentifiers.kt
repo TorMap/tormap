5ffbc3c361df1901fbecb37663e5db832175ed67
Julius Henke <mail@juliushenke.com>
2021-08-23 14:18:47 +0200

    Add node family identifiers endpoint
    Former-commit-id: f90c435a4d309e56fe3dbed98c84ca41b165f634

---
+++ backend/src/main/kotlin/org/tormap/adapter/controller/view/NodeFamilyIdentifiers.kt
@@ -0,0 +1,13 @@
+@file:Suppress("unused")
+
+package org.tormap.adapter.controller.view
+
+/**
+ * Identifiers of a family
+ */
+class NodeFamilyIdentifiers(
+    val id: Long,
+    val memberCount: Long,
+    val fingerprints: String,
+    val nicknames: String,
+)
