e5845c931ebe4141cf4758e2b6b8739b23b71f14
Julius Henke <mail@juliushenke.com>
2021-08-25 13:45:10 +0200

    Remove AS from node details
    Former-commit-id: f682fce0c7d87fc24a13bf3740a2d8af1a74187e

--- backend/src/main/kotlin/org/tormap/database/entity/NodeDetails.kt
+++ backend/src/main/kotlin/org/tormap/database/entity/NodeDetails.kt
@@ -1,6 +1,8 @@
 package org.tormap.database.entity
 
+import org.tormap.calculateIPv4NumberRepresentation
 import org.tormap.jointToCommaSeparated
+import org.tormap.logger
 import org.tormap.stripLengthForDB
 import org.torproject.descriptor.ServerDescriptor
 import java.time.LocalDate
@@ -17,7 +19,6 @@ import javax.persistence.*
         Index(columnList = "fingerprint, month", unique = true, name = "fingerprint_month_index"),
         Index(columnList = "nickname, month", name = "nickname_month_index"),
         Index(columnList = "familyId", name = "familyId_index"),
-        Index(columnList = "autonomousSystemNumber", name = "autonomousSystemNumber_number_index"),
     ]
 )
 class NodeDetails(
@@ -28,15 +29,16 @@ class NodeDetails(
     @Id
     @GeneratedValue
     val id: Long? = null,
-
-    @Column(length = 255)
-    var autonomousSystemName: String? = null,
-
-    @Column(length = 10)
-    var autonomousSystemNumber: String? = null,
 ) {
     @Column(length = 15)
     var address: String? = descriptor.address
+
+    var addressNumber: Long? = try {
+        calculateIPv4NumberRepresentation(descriptor.address)
+    } catch (exception: Exception) {
+        logger().warn("Could not calculate addressNumber for address ${descriptor.address}")
+        null
+    }
 
     var allowSingleHopExits: Boolean = descriptor.allowSingleHopExits
 
