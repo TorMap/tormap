0a621d7fd576081760e24a37ab5bc3a8b06bee23
Julius Henke <mail@juliushenke.com>
2021-08-28 00:53:50 +0200

    Fix which node months need to be updated after processing
    Former-commit-id: c15ea34231ce5395c2e2e6965b05c69205890c58

--- backend/src/main/kotlin/org/tormap/database/repository/NodeDetailsRepositoryImpl.kt
+++ backend/src/main/kotlin/org/tormap/database/repository/NodeDetailsRepositoryImpl.kt
@@ -17,11 +17,10 @@ interface NodeDetailsRepositoryImpl : NodeDetailsRepository {
     )
     fun findNodeIdentifiers(ids: List<Long>): List<NodeIdentifiers>
 
-    @Query("SELECT new org.tormap.adapter.controller.view.NodeFamilyIdentifiers(n.familyId, count(n.id), function('LISTAGG', n.fingerprint, ', '), function('LISTAGG', n.nickname, ', '), function('LISTAGG', a.autonomousSystemName, ', ')) " +
-            "FROM NodeDetails n " +
-            "LEFT JOIN AutonomousSystem a ON a.ipRange.ipFrom <= n.addressNumber and a.ipRange.ipTo >= n.addressNumber " +
-            "WHERE n.familyId in :ids " +
-            "GROUP BY n.familyId"
+    @Query("SELECT new org.tormap.adapter.controller.view.NodeFamilyIdentifiers(familyId, count(id), function('LISTAGG', nickname, ', '), function('LISTAGG', autonomousSystemName, ', ')) " +
+            "FROM NodeDetails " +
+            "WHERE familyId in :ids " +
+            "GROUP BY familyId"
     )
     fun findFamilyIdentifiers(ids: List<Long>): List<NodeFamilyIdentifiers>
 }
