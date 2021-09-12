298e3bfce406220fd37f105a951589379948caac
Julius Henke <mail@juliushenke.com>
2021-09-12 16:33:58 +0200

    Use correct backend URL in production, Setup docker image deploy

--- frontend/src/util/config.ts
+++ frontend/src/util/config.ts
@@ -1,7 +1,7 @@
 import {Settings} from "../types/app-state";
 import {RelayFlag, RelayType} from "../types/relay";
 
-export const apiBaseUrl = "http://localhost:8080"
+export const apiBaseUrl = process.env.REACT_APP_BACKEND_API_URL
 
 // Default settings, that are selected on page load
 export const defaultSettings: Settings = {
