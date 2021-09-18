39aa3c30cd3a5e5eca9a8605a746276ec0f488ad
Julius Henke <mail@juliushenke.com>
2021-09-19 01:59:32 +0200

    Edit about-information.tsx

--- frontend/src/components/about-information.tsx
+++ frontend/src/components/about-information.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import {
+    Box,
     Button,
     Card,
     CardHeader,
@@ -14,15 +15,15 @@ import {
     ListItemIcon,
     ListItemText,
     makeStyles,
-    Paper,
     Typography
 } from "@material-ui/core";
 import GitHubIcon from '@material-ui/icons/GitHub';
+import EmailIcon from '@material-ui/icons/Email';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import CloseIcon from "@material-ui/icons/Close";
-import panda from "../data/about-logos/panda-logo-300x113-white.png";
 import LanguageIcon from '@material-ui/icons/Language';
 import InfoIcon from '@material-ui/icons/Info';
+import {Icon} from "@iconify/react";
 
 /**
  * Styles according to Material UI doc for components used in AppSettings component
@@ -48,10 +49,13 @@ const useStyle = makeStyles(() => ({
     image: {
         display: "block",
         margin: "auto",
-        width: "80%",
-        padding: "10px",
+        height: "25px",
         textAlign: "center",
     },
+    iconLink: {
+        marginTop: "5px",
+        marginLeft: "15px",
+    }
 }))
 
 /**
@@ -69,8 +73,11 @@ export const AboutInformation: React.FunctionComponent = () => {
             </Button>
             <Dialog open={showDialog} fullWidth={true} maxWidth={"md"} onBackdropClick={() => setShowDialog(false)}>
                 <DialogTitle>
-                    <Typography
-                        variant="h6">About TorMap</Typography>
+                    <Box display="flex">
+                        <Typography variant="h6">
+                            About TorMap
+                        </Typography>
+                    </Box>
                     <IconButton aria-label="close" className={classes.closeButton} onClick={() => setShowDialog(false)}>
                         <CloseIcon/>
                     </IconButton>
@@ -83,7 +90,7 @@ export const AboutInformation: React.FunctionComponent = () => {
                         daily. The nonprofit organization <Link href={"https://www.torproject.org/"}
                                                                 target={"_blank"}>TorProject</Link> already provides
                         a large <Link href={"https://metrics.torproject.org/collector.html"}
-                                 target={"_blank"}>archive</Link> with raw historic data about the network.<br/>
+                                      target={"_blank"}>archive</Link> with raw historic data about the network.<br/>
                         With our app TorMap we visualize, group and filter public Tor relays on a world
                         map. The state of the network can be viewed for any day between October 2007 and today. Getting
                         details like IP address, contact or Autonomous System info of a relay is as easy as selecting it
@@ -99,31 +106,55 @@ export const AboutInformation: React.FunctionComponent = () => {
                         Darmstadt which is funded/grant-aided by the <Link href={"https://panda-projekt.de/"}
                                                                            target={"_blank"}>BMBF</Link>.
                         The interdisciplinary team behind the project is researching the Darknet with a view on
-                        technical-, social- and philosophical since.
+                        technical-, social- and philosophical science.
                         The goal of PANDA is to contribute on improving the fight against crime in the Darknet within
                         the
                         framework of civil security research, but without affecting its legitimate uses or even
                         anonymous communication as a whole.
                     </Typography>
-                </DialogContent>
-                <DialogContent
-                    dividers
-                >
                     <Grid container spacing={3}>
-                        <Grid item xs={4}>
+                        <Grid item xs={3}>
+                            <Card elevation={20}>
+                                <CardHeader
+                                    title="TorMap"
+                                />
+                                <List dense={true}>
+                                    <ListItem button component="a" href={"https://github.com/TorMap/tormap"}
+                                              target={"_blank"}>
+                                        <ListItemIcon>
+                                            <GitHubIcon/>
+                                        </ListItemIcon>
+                                        <ListItemText>
+                                            Github
+                                        </ListItemText>
+                                    </ListItem>
+                                    <ListItem button component="a" href={"mailto:hi@tormap.org"}
+                                              target={"_blank"}>
+                                        <ListItemIcon>
+                                            <EmailIcon/>
+                                        </ListItemIcon>
+                                        <ListItemText>
+                                            Email
+                                        </ListItemText>
+                                    </ListItem>
+                                    <ListItem button component="a" href={"https://tippin.me/@Julius_Henke"}
+                                              target={"_blank"}>
+                                        <ListItemIcon>
+                                            <Icon icon="mdi:bitcoin" width={26}/>
+                                        </ListItemIcon>
+                                        <ListItemText>
+                                            Donate
+                                        </ListItemText>
+                                    </ListItem>
+                                </List>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={3}>
                             <Card elevation={20}>
                                 <CardHeader
                                     title="Julius Henke"
                                 />
                                 <List dense={true}>
-                                    <ListItem button component="a" href="https://juliushenke.com/" target={"_blank"}>
-                                        <ListItemIcon>
-                                            <LanguageIcon/>
-                                        </ListItemIcon>
-                                        <ListItemText>
-                                            Website
-                                        </ListItemText>
-                                    </ListItem>
                                     <ListItem button component="a" href="https://github.com/JuliusHenke"
                                               target={"_blank"}>
                                         <ListItemIcon>
@@ -133,10 +164,18 @@ export const AboutInformation: React.FunctionComponent = () => {
                                             GitHub
                                         </ListItemText>
                                     </ListItem>
+                                    <ListItem button component="a" href="https://juliushenke.com/" target={"_blank"}>
+                                        <ListItemIcon>
+                                            <LanguageIcon/>
+                                        </ListItemIcon>
+                                        <ListItemText>
+                                            Website
+                                        </ListItemText>
+                                    </ListItem>
                                     <ListItem button component="a" href="https://twitter.com/Julius_Henke"
                                               target={"_blank"}>
                                         <ListItemIcon>
-                                            <TwitterIcon />
+                                            <TwitterIcon/>
                                         </ListItemIcon>
                                         <ListItemText>
                                             Twitter
@@ -145,7 +184,7 @@ export const AboutInformation: React.FunctionComponent = () => {
                                 </List>
                             </Card>
                         </Grid>
-                        <Grid item xs={4}>
+                        <Grid item xs={3}>
                             <Card elevation={20}>
                                 <CardHeader
                                     title="Tim Kilb"
@@ -171,26 +210,18 @@ export const AboutInformation: React.FunctionComponent = () => {
                                 </List>
                             </Card>
                         </Grid>
-                        <Grid item xs={4}>
-                            <Link href={"https://panda-projekt.de/"} target={"_blank"}>
-                                <Paper elevation={20} className={classes.paper}>
-                                    <img
-                                        src={panda}
-                                        alt={"https://panda-projekt.de/"}
-                                        className={classes.image}
-                                    />
-                                </Paper>
-                            </Link>
-                        </Grid>
                     </Grid>
                 </DialogContent>
                 <DialogContent
                     dividers
                 >
-                    <Typography variant={"body2"}>This site includes IP2Location LITE data available from <Link
-                        href={"https://www.ip2location.com/"} target={"_blank"}>https://www.ip2location.com/</Link> as
+                    <Typography variant={"body2"}>
+                        This site includes IP2Location LITE data available from <Link
+                        href={"https://www.ip2location.com/"}
+                        target={"_blank"}>https://www.ip2location.com/</Link> as
                         well as GeoJSON data from <Link href={"https://geojson-maps.ash.ms/"}
-                                                        target={"_blank"}>https://geojson-maps.ash.ms/</Link>.</Typography>
+                                                        target={"_blank"}>https://geojson-maps.ash.ms/</Link>.
+                    </Typography>
                 </DialogContent>
             </Dialog>
         </div>
