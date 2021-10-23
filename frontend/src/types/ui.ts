c543147808abfab43e72debd99fa7c5db2cc86d5
Julius Henke <mail@juliushenke.com>
2021-10-23 12:40:12 +0200

    Add retry action to snackbar

--- frontend/src/types/ui.ts
+++ frontend/src/types/ui.ts
@@ -1,8 +1,8 @@
 import {Dialog, withStyles} from "@material-ui/core";
 
 export enum SnackbarMessage {
-    ConnectionFailed = "Connection to backend server failed!",
-    NoRelaysWithFlags = "There are no relays with the filtered flags!",
+    ConnectionFailed = "Connection failed! Maybe the server is being upgraded.",
+    NoRelaysWithFlags = "There are no relays with for the current settings!",
     NoFamilyData = "There are no families for this date! This data might be available soon.",
     NoRelayDetails = "Currently we do not have more information about this relay in this month.",
 }
