07e3cd3a67192b5ea62e6c5f766f0f47ad6eeae1
Julius Henke <mail@juliushenke.com>
2021-10-23 13:16:59 +0200

    Change snackbar messages

--- frontend/src/types/ui.ts
+++ frontend/src/types/ui.ts
@@ -2,9 +2,9 @@ import {Dialog, withStyles} from "@material-ui/core";
 
 export enum SnackbarMessage {
     ConnectionFailed = "Connection failed! Maybe the server is being upgraded.",
-    NoRelaysWithFlags = "There are no relays with for the current settings!",
-    NoFamilyData = "There are no families for this date! This data might be available soon.",
-    NoRelayDetails = "Currently we do not have more information about this relay in this month.",
+    NoRelaysWithFlags = "There are no relays for the current settings!",
+    NoFamilyData = "Currently we have no family information for this date!",
+    NoRelayDetails = "Currently we have no details about this relay for this month.",
 }
 
 export const FullHeightDialog = withStyles(() => ({
