90d53877c6ef3125800f2855faf36a969fab841c
Julius Henke <mail@juliushenke.com>
2021-11-19 15:00:16 +0100

    Add authenticated remote DB access

--- backend/src/main/kotlin/org/tormap/config/SecurityConfig.kt
+++ backend/src/main/kotlin/org/tormap/config/SecurityConfig.kt
@@ -1,5 +1,6 @@
 package org.tormap.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -7,13 +8,31 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig : WebSecurityConfigurerAdapter() {
+class SecurityConfig(
+    @Value("\${spring.h2.console.path}")
+    private val h2ConsolePath: String,
+
+    @Value("\${management.endpoints.web.base-path}")
+    private val actuatorPath: String,
+) : WebSecurityConfigurerAdapter() {
 
     @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
+        // Require authenticated users for actuator endpoints
         http.authorizeRequests()
-            .antMatchers("/actuator/**")
+            .antMatchers("$actuatorPath/**")
             .authenticated()
+
+        // Require authenticated users for H2 database web console
+        http.authorizeRequests()
+            .antMatchers("$h2ConsolePath/**")
+            .authenticated()
+            .and()
+            .csrf().disable()
+            .headers().frameOptions().disable()
+
+        // Enable unauthenticated access to all other endpoints and add a form login for authenticated ones
+        http.authorizeRequests()
             .anyRequest().permitAll()
             .and()
             .formLogin()
