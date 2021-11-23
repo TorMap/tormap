6e5487fac79d0b08c3fb02c0ed424126374e7d2a
Julius Henke <mail@juliushenke.com>
2021-11-23 23:30:09 +0100

    Fix ApiTrace entity, Ensure location lookup is complete

--- backend/src/main/kotlin/org/tormap/database/entity/ApiTrace.kt
+++ backend/src/main/kotlin/org/tormap/database/entity/ApiTrace.kt
@@ -2,14 +2,21 @@ package org.tormap.database.entity
 
 import org.springframework.boot.actuate.trace.http.HttpTrace
 import java.time.Instant
-import javax.persistence.Entity
-import javax.persistence.Enumerated
-import javax.persistence.Id
+import javax.persistence.*
 
 @Suppress("unused")
 @Entity
+@Table(
+    indexes = [
+        Index(columnList = "timestamp", name = "timestamp_index"),
+        Index(columnList = "method", name = "method_index"),
+        Index(columnList = "responseStatus", name = "responseStatus_index"),
+    ]
+)
 class ApiTrace(trace: HttpTrace) {
     @Id
+    @GeneratedValue
+    val id: Long? = null
     var timestamp: Instant = trace.timestamp
     var uri: String = trace.request.uri.toString()
 
@@ -17,16 +24,20 @@ class ApiTrace(trace: HttpTrace) {
     var method: RequestMethod = RequestMethod.valueOf(trace.request.method.uppercase())
     var responseStatus: Int = trace.response.status
     var userAgent: String? = trace.request.headers["user-agent"]?.firstOrNull()
+    var timeTaken: Long = trace.timeTaken
+
+    @Column(length = 2, columnDefinition = "char(2)")
+    var countryCode: String? = null
 }
 
-enum class RequestMethod(method: String) {
-    GET("GET"),
-    HEAD("HEAD"),
-    POST("POST"),
-    PUT("PUT"),
-    DELETE("DELETE"),
-    CONNECT("CONNECT"),
-    OPTIONS("OPTIONS"),
-    TRACE("TRACE"),
-    PATCH("PATCH"),
+enum class RequestMethod {
+    GET,
+    HEAD,
+    POST,
+    PUT,
+    DELETE,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
 }
