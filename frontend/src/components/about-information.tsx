12ebc86056ffe330dd758d88e6f8adf4d03f4f46
Julius Henke <mail@juliushenke.com>
2021-11-30 02:56:10 +0100

    Update about-information.tsx

--- frontend/src/components/about-information.tsx
+++ frontend/src/components/about-information.tsx
@@ -7,7 +7,7 @@ import {
     CardHeader,
     Dialog,
     DialogContent,
-    DialogTitle,
+    DialogTitle, Divider,
     Grid,
     IconButton,
     Link,
@@ -79,10 +79,8 @@ export const AboutInformation: React.FunctionComponent = () => {
             <Dialog open={showDialog} fullWidth={true} maxWidth={"md"} onBackdropClick={() => setShowDialog(false)}>
                 <DialogTitle>
                     <Box display="flex" alignItems={"center"}>
-                        <Avatar className={classes.logo} src={TorMapLogo} alt={"TorMap logo"} />
-                        <Typography variant="h6">
-                            About TorMap
-                        </Typography>
+                        <Avatar className={classes.logo} src={TorMapLogo} alt={"TorMap logo"}/>
+                        <Typography variant="h5">TorMap</Typography>
                     </Box>
                     <IconButton aria-label="close" className={classes.closeButton} onClick={() => setShowDialog(false)}>
                         <CloseIcon/>
@@ -91,33 +89,37 @@ export const AboutInformation: React.FunctionComponent = () => {
                 <DialogContent
                     dividers
                 >
+                    <Typography variant={"h6"}>What can I do here?</Typography>
                     <Typography variant={"body1"} gutterBottom>
-                        The Tor network currently consists of thousands of nodes which route anonymous internet traffic
-                        daily. The nonprofit organization <Link href={"https://www.torproject.org/"}
-                                                                target={"_blank"}>TorProject</Link> already provides
-                        a large <Link href={"https://metrics.torproject.org/collector.html"}
-                                      target={"_blank"}>archive</Link> with raw historic data about the network.<br/>
-                        With our app TorMap we visualize, group and filter public Tor relays on a world
-                        map. The state of the network can be viewed for any day between October 2007 and today. Getting
-                        details like IP address, contact or Autonomous System info of a relay is as easy as selecting it
-                        on the map.<br/>
-                        The project was developed by two students for their practical P4-Projekt at the <Link
+                        TorMap is a world map displaying approximate locations where Tor relays are being hosted. The
+                        Tor network currently consists of thousands of relays which route anonymous internet traffic
+                        daily. With our app you can group, filter and analyze Tor relays. The historic state of the
+                        network can be viewed for any day between October 2007 and today.<br/>
+                        We developed TorMap for our practical P4-Project at the <Link
                         href={"https://www.tu-darmstadt.de/"} target={"_blank"}>Technical University of
                         Darmstadt</Link> as part of the <Link href={"https://panda-projekt.de/"}
-                                                              target={"_blank"}>PANDA project</Link> and supervised
+                                                              target={"_blank"}>PANDA project</Link>. It was supervised
                         by <Link
-                        href={"mailto:florian.platzer@sit.fraunhofer.de"} target={"_blank"}>Florian Platzer</Link>.<br/>
-                        PANDA is a joint project by the <Link href={"https://www.sit.fraunhofer.de/"} target={"_blank"}>Frauenhofer
-                        SIT</Link> and the TU
-                        Darmstadt which is funded/grant-aided by the <Link href={"https://www.bmbf.de/"}
-                                                                           target={"_blank"}>BMBF</Link>.
-                        The interdisciplinary team behind the project is researching the Darknet with a view on
-                        technical-, social- and philosophical science.
-                        The goal of PANDA is to contribute on improving the fight against crime in the Darknet within
-                        the
-                        framework of civil security research, but without affecting its legitimate uses or even
-                        anonymous communication as a whole.
+                        href={"mailto:florian.platzer@sit.fraunhofer.de"} target={"_blank"}>Florian
+                        Platzer</Link> from <Link href={"https://www.sit.fraunhofer.de/"} target={"_blank"}>Frauenhofer
+                        SIT</Link>.
                     </Typography>
+                    <Typography variant={"h6"}>How do we get our data?</Typography>
+                    <Typography variant={"body1"} gutterBottom>
+                        The nonprofit organization <Link href={"https://www.torproject.org/"}
+                                                         target={"_blank"}>TorProject</Link> already provides
+                        a large <Link href={"https://metrics.torproject.org/collector.html"}
+                                      target={"_blank"}>archive</Link> with raw historic data about the network.
+                        We regularly process these so called "descriptors" and lookup IPv4 addresses to get geo locations and
+                        autonomous systems. If you are interested in our implementation, check out the open source repository
+                        on <Link href={"https://github.com/TorMap/tormap"}
+                                 target={"_blank"}>GitHub</Link>.<br/>
+                        The location and ownership of IP ranges can change over time. Since we
+                        only use current IP data, some relays might have been hosted somewhere else, than displayed on
+                        our world map.
+                    </Typography>
+                    <Divider/>
+                    <br/>
                     <Grid container spacing={3}>
                         <Grid item xs={3}>
                             <Card elevation={20}>
@@ -222,12 +224,12 @@ export const AboutInformation: React.FunctionComponent = () => {
                     dividers
                 >
                     <Typography variant={"body2"}>
-                        We use <Link href={"https://db-ip.com"} target={"_blank"}>IP Geolocation data by DB-IP</Link>,
-                        <Link href={"https://www.maxmind.com/"} target={"_blank"}>Autonomous System data by
-                            MaxMind</Link>
-                        and GeoJSON data from
-                        <Link href={"https://geojson-maps.ash.ms/"}
-                              target={"_blank"}>https://geojson-maps.ash.ms/</Link>.
+                        We use IP geolocation data by <Link href={"https://db-ip.com"}
+                                                            target={"_blank"}>DB-IP</Link>{", "}
+                        Autonomous System data by <Link href={"https://www.maxmind.com/"}
+                                                        target={"_blank"}>MaxMind</Link>{" "}
+                        and GeoJSON data from <Link href={"https://geojson-maps.ash.ms/"}
+                                                    target={"_blank"}>https://geojson-maps.ash.ms/</Link>.
                     </Typography>
                 </DialogContent>
             </Dialog>
