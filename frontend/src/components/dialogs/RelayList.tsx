369fc2bba662c15fcbccc3fd1f249c3091fab9e1
Julius Henke <mail@juliushenke.com>
2021-12-03 03:26:31 +0100

    Rework the RelayDetailsDialogLarge.tsx, Add sorting

--- frontend/src/components/dialogs/RelayList.tsx
+++ frontend/src/components/dialogs/RelayList.tsx
@@ -1,19 +1,13 @@
 import React, {FunctionComponent} from "react";
-import {List, ListItem, ListItemIcon, ListItemText, Tooltip} from "@mui/material";
+import {List, ListItem, ListItemIcon, ListItemText} from "@mui/material";
 import {getIcon} from "../../types/icons";
-import {getRelayType} from "../../util/aggregate-relays";
-import {RelayLocationDto, RelayIdentifierDto} from "../../types/responses";
+import {RelayMatch} from "./RelayDetailsDialogLarge";
 
 interface Props {
     /**
-     * Relays which the user can view detailed information about
-     */
-    relays: RelayLocationDto[]
-
-    /**
      * Relay Identifiers
      */
-    relayIdentifiers: RelayIdentifierDto[]
+    relayMatches: RelayMatch[]
 
     /**
      * ID of currently selected Relay
@@ -29,46 +23,30 @@ interface Props {
 /**
  * A List with Relays to select one
  * @param relays
- * @param relayIdentifiers
+ * @param relayMatches
  * @param relayDetailsId
  * @param setRelayDetailsId
  * @constructor
  */
 export const RelayList: FunctionComponent<Props> = ({
-                                                        relays,
-                                                        relayIdentifiers,
+                                                        relayMatches,
                                                         relayDetailsId,
                                                         setRelayDetailsId,
                                                     }) => {
-
-    const findIcon = (identifier: RelayIdentifierDto) => {
-        const relay = relays.find(
-            (relay) => relay.detailsId === identifier.id
-        )
-        return relay ? getIcon(getRelayType(relay)) : null
-    }
-
     return (
         <List>
-            {relayIdentifiers.map((identifier) =>
-                (identifier.id &&
-                    <Tooltip
-                        key={identifier.id}
-                        title={identifier.fingerprint}
-                        arrow={true}
-                        sx={{maxWidth: "none",}}
-                    >
+            {relayMatches.map((relayMatch) =>
+                (relayMatch.id &&
                         <ListItem
                             button={true}
-                            selected={identifier.id === relayDetailsId}
-                            onClick={() => setRelayDetailsId(identifier.id)}
+                            selected={relayMatch.id === relayDetailsId}
+                            onClick={() => setRelayDetailsId(relayMatch.id)}
                         >
                             <ListItemIcon>
-                                {findIcon(identifier)}
+                                {getIcon(relayMatch.relayType)}
                             </ListItemIcon>
-                            <ListItemText primary={identifier.nickname}/>
+                            <ListItemText primary={relayMatch.nickname}/>
                         </ListItem>
-                    </Tooltip>
                 )
             )}
         </List>
