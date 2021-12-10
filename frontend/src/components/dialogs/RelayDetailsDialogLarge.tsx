18da2003c7d7c0680628601e07015685fd653b8b
Tim Kilb <timkilb@outlook.de>
2021-12-10 17:39:20 +0100

    error fixed

--- frontend/src/components/dialogs/RelayDetailsDialogLarge.tsx
+++ frontend/src/components/dialogs/RelayDetailsDialogLarge.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useMemo, useState} from "react";
+import React from "react";
 import {
     Box,
     CircularProgress,
@@ -7,25 +7,19 @@ import {
     FormControl,
     Grid,
     IconButton,
-    Link,
     MenuItem,
     Select,
-    Typography,
-    useMediaQuery,
-    useTheme
+    Typography
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import {getIcon} from "../../types/icons";
 import {getRelayType} from "../../util/aggregate-relays";
-import {DetailsInfo, RelayDetailsDto, RelayIdentifierDto, RelayLocationDto} from "../../types/responses";
-import {FullHeightDialog, SnackbarMessage} from "../../types/ui";
-import {RelayFlag, RelayFlagLabel, RelayType} from "../../types/relay";
-import {backend} from "../../util/util";
-import {useSnackbar} from "notistack";
+import {RelayIdentifierDto, RelayLocationDto} from "../../types/responses";
+import {FullHeightDialog} from "../../types/ui";
+import {RelayType} from "../../types/relay";
 import {RelayList} from "./RelayList";
 import {RelayDetails} from "./RelayDetails";
-import {SelectChangeEvent} from "@mui/material/Select/SelectInput";
-import {DetailsProps, formatBoolean, formatBytesToMBPerSecond, formatSecondsToHours} from "./DetailsDialogUtil";
+import {DetailsDialogProps} from "./DetailsDialogUtil";
 
 
 /**
@@ -35,170 +29,20 @@ import {DetailsProps, formatBoolean, formatBytesToMBPerSecond, formatSecondsToHo
  * @param relays - Selectable relays
  * @param enqueueSnackbar - The event handler for showing a snackbar message
  */
-export const RelayDetailsDialogLarge: React.FunctionComponent<DetailsProps> = ({
+export const RelayDetailsDialogLarge: React.FunctionComponent<DetailsDialogProps> = ({
                                                                             showDialog,
                                                                             closeDialog,
                                                                             relays,
+                                                                        relayIdentifiers,
+                                                                        sortRelaysBy,
+                                                                        handleSelectSortByChange,
+                                                                        rawRelayDetails,
+                                                                        setRelayDetailsId,
+                                                                        sortedRelayMatches,
+                                                                        relayDetailsId,
+                                                                        relayDetails,
+                                                                        relay,
                                                                         }) => {
-    const [relayIdentifiers, setRelayIdentifiers] = useState<RelayIdentifierDto[]>([])
-    const [relayDetailsId, setRelayDetailsId] = useState<number>()
-    const [rawRelayDetails, setRawRelayDetails] = useState<RelayDetailsDto>()
-    const [relayDetails, setRelayDetails] = useState<DetailsInfo[]>()
-    const [sortRelaysBy, setSortRelaysBy] = useState<keyof RelayMatch>("relayType")
-
-    const {enqueueSnackbar} = useSnackbar();
-
-    //Todo: duplicate code
-    const relayDetailsIdToLocationMap = useMemo(() => {
-        const relayDetailsIdToLocationMap = new Map<number, RelayLocationDto>()
-        relays.filter(relay => relay.detailsId).forEach(relay => relayDetailsIdToLocationMap.set(relay.detailsId!!, relay))
-        return relayDetailsIdToLocationMap
-    }, [relays])
-
-    //Todo: duplicate code
-    const relayMatches = useMemo(
-        () => {
-            const relayMatches: RelayMatch[] = [];
-            relayIdentifiers.forEach(identifier => {
-                const relayLocation = relayDetailsIdToLocationMap.get(identifier.id)
-                if (relayLocation) {
-                    relayMatches.push({
-                        ...identifier,
-                        location: relayLocation,
-                        relayType: getRelayType(relayLocation)
-                    })
-                }
-            })
-            return relayMatches.sort((a, b) => a.relayType > b.relayType ? 1 : -1)
-        },
-        [relayIdentifiers, relayDetailsIdToLocationMap]
-    )
-
-    //Todo: duplicate code
-    const sortedRelayMatches = useMemo(
-        () =>  relayMatches.sort((a, b) => a[sortRelaysBy] > b[sortRelaysBy] ? 1 : -1),
-        [relayMatches, sortRelaysBy]
-    )
-
-    //Todo: duplicate code
-    /**
-     * Query relayIdentifiers for relays from backend
-     */
-    useEffect(() => {
-        setRelayDetailsId(undefined)
-        setRawRelayDetails(undefined)
-        setRelayDetails(undefined)
-        setRelayIdentifiers([])
-        if (relays.length > 0 && relayDetailsIdToLocationMap.size === 0) {
-            enqueueSnackbar(SnackbarMessage.NoRelayDetails, {variant: "warning"})
-            closeDialog()
-        } else if (relayDetailsIdToLocationMap.size === 1) {
-            setRelayDetailsId(relayDetailsIdToLocationMap.keys().next().value)
-        } else if (relayDetailsIdToLocationMap.size > 1) {
-            backend.post<RelayIdentifierDto[]>(
-                '/relay/details/relay/identifiers',
-                Array.from(relayDetailsIdToLocationMap.keys())
-            ).then(response => {
-                const requestedRelayIdentifiers = response.data
-                setRelayIdentifiers(requestedRelayIdentifiers)
-            }).catch(() => {
-                enqueueSnackbar(SnackbarMessage.ConnectionFailed, {variant: "error"})
-                closeDialog()
-            })
-        }
-
-    }, [closeDialog, relays, enqueueSnackbar, relayDetailsIdToLocationMap])
-
-    //Todo: duplicate code
-    /**
-     * Query more information for the selected relay
-     */
-    useEffect(() => {
-        function constructFlagString(flags: RelayFlag[] | null | undefined): string {
-            if (flags) {
-                return flags.map(flag => RelayFlagLabel[flag]).join(", ")
-            }
-            return "no flags assigned"
-        }
-
-        if (!relayDetailsId && sortedRelayMatches.length > 0) {
-            setRelayDetailsId(sortedRelayMatches[0].id)
-        } else if (relayDetailsId) {
-            backend.get<RelayDetailsDto>(`/relay/details/relay/${relayDetailsId}`).then(response => {
-                const relay = response.data
-                setRawRelayDetails(relay)
-                setRelayDetails([
-                    {
-                        name: "Fingerprint",
-                        value:
-                            <Link
-                                href={`https://metrics.torproject.org/rs.html#details/${relay.fingerprint}`}
-                                target={"_blank"}>
-                                {relay.fingerprint}
-                            </Link>
-                    },
-                    {
-                        name: "IP address",
-                        value:
-                            <Link
-                                href={`https://metrics.torproject.org/rs.html#search/${relay.address}`}
-                                target={"_blank"}>
-                                {relay.address}
-                            </Link>
-                    },
-                    {
-                        name: "Flags assigned by authorities",
-                        value: constructFlagString(relayDetailsIdToLocationMap.get(relay.id)?.flags)
-                    },
-                    {name: "Autonomous System", value: relay.autonomousSystemName},
-                    {
-                        name: "Autonomous System Number",
-                        value:
-                            <Link
-                                href={`https://metrics.torproject.org/rs.html#search/as:${relay.autonomousSystemNumber}`}
-                                target={"_blank"}>
-                                {relay.autonomousSystemNumber}
-                            </Link>
-                    },
-                    {name: "Platform", value: relay.platform},
-                    {name: "Uptime", value: formatSecondsToHours(relay.uptime)},
-                    {name: "Contact", value: relay.contact},
-                    {name: "Bandwidth for short intervals", value: formatBytesToMBPerSecond(relay.bandwidthBurst)},
-                    {name: "Bandwidth for long periods", value: formatBytesToMBPerSecond(relay.bandwidthRate)},
-                    {name: "Bandwidth observed", value: formatBytesToMBPerSecond(relay.bandwidthObserved)},
-                    {name: "Supported protocols", value: relay.protocols},
-                    {name: "Allows single hop exit", value: formatBoolean(relay.allowSingleHopExits)},
-                    {name: "Is hibernating", value: formatBoolean(relay.isHibernating)},
-                    {name: "Caches extra info", value: formatBoolean(relay.cachesExtraInfo)},
-                    {name: "Is a hidden service directory", value: formatBoolean(relay.isHiddenServiceDir)},
-                    {name: "Accepts tunneled directory requests", value: formatBoolean(relay.tunnelledDirServer)},
-                    {name: "Link protocol versions", value: relay.linkProtocolVersions},
-                    {name: "Circuit protocol versions", value: relay.circuitProtocolVersions},
-                    {name: "Self reported family members", value: relay.familyEntries},
-                    {name: "Infos published by relay on", value: relay.day},
-                ])
-            })
-                .catch(() => {
-                    enqueueSnackbar(SnackbarMessage.ConnectionFailed, {variant: "error"})
-                })
-        }
-    }, [sortedRelayMatches, relayDetailsId, relayDetailsIdToLocationMap, enqueueSnackbar])
-
-    const theme = useTheme()
-    const desktop = useMediaQuery(theme.breakpoints.up("lg"))
-    //Todo: duplicate code
-    const relay = relays.find((relay) => relayDetailsId && relay.detailsId === relayDetailsId)
-
-    //Todo: duplicate code
-    const handeSelectSortByChange = (event: SelectChangeEvent<keyof RelayMatch>) => {
-        switch (event.target.value) {
-            case "nickname": setSortRelaysBy("nickname")
-                break
-            default: setSortRelaysBy("relayType")
-                break
-        }
-    }
-
     return (
         <FullHeightDialog
             open={showDialog}
@@ -206,7 +50,6 @@ export const RelayDetailsDialogLarge: React.FunctionComponent<DetailsProps> = ({
             onBackdropClick={closeDialog}
             maxWidth={relays.length > 1 ? "lg" : "md"}
             fullWidth={true}
-            fullScreen={!desktop}
             sx={{
                 paper: {
                     height: '70vh',
@@ -223,7 +66,7 @@ export const RelayDetailsDialogLarge: React.FunctionComponent<DetailsProps> = ({
                             <Select
                                 value={sortRelaysBy}
                                 label="Sort by"
-                                onChange={handeSelectSortByChange}
+                                onChange={handleSelectSortByChange}
                             >
                                 <MenuItem value={"relayType"}>Type</MenuItem>
                                 <MenuItem value={"nickname"}>Nickname</MenuItem>
