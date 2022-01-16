2576c978734c50202432c3bca16ec9e1a7b95d14
Julius Henke <mail@juliushenke.com>
2022-01-16 20:38:47 +0100

    Add GZIP compression for API responses, Fix slider marks and historic data processing notice

--- frontend/src/context/date-context.tsx
+++ frontend/src/context/date-context.tsx
@@ -47,7 +47,7 @@ export const DateProvider: FunctionComponent<DateProviderProps> = ({children}) =
         if (availableDays.length > 0) {
             setSelectedDate(availableDays[availableDays.length - 1])
         }
-        const expectedNumberOfDays = differenceInDays(new Date("2007-10-27"), new Date())
+        const expectedNumberOfDays = Math.abs(differenceInDays(new Date("2007-10-27"), new Date()))
         if (availableDays.length < expectedNumberOfDays) {
             enqueueSnackbar(SnackbarMessage.HistoricDataProcessing, {variant: "info"})
         }
