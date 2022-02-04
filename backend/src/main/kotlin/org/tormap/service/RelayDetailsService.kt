403916a847a952db0e6aa40f139391ead9711c6b
Julius Henke <mail@juliushenke.com>
2022-02-04 22:17:52 +0100

    Convert autonomousSystemNumber from Long to Int

--- backend/src/main/kotlin/org/tormap/service/RelayDetailsService.kt
+++ backend/src/main/kotlin/org/tormap/service/RelayDetailsService.kt
@@ -94,7 +94,7 @@ class RelayDetailsService(
             val autonomousSystem = ipLookupService.lookupAutonomousSystem(this.address!!)
             if (autonomousSystem != null) {
                 this.autonomousSystemName = autonomousSystem.autonomousSystemOrganization
-                this.autonomousSystemNumber = autonomousSystem.autonomousSystemNumber
+                this.autonomousSystemNumber = autonomousSystem.autonomousSystemNumber.toInt()
                 relayDetailsRepositoryImpl.save(this)
                 return true
             }
