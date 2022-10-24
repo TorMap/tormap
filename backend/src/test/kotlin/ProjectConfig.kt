971ef7a918653e11e6dbce9a33dfee49889f4b17
Julius Henke <mail@juliushenke.com>
2022-10-24 20:37:47 +0200

    test: optimize imports

--- backend/src/test/kotlin/ProjectConfig.kt
+++ backend/src/test/kotlin/ProjectConfig.kt
@@ -4,7 +4,6 @@ import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.extensions.spring.SpringExtension
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
-import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
