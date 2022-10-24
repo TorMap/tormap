c849181e5e26cfa4e4e022d704aec2ec8d4428d8
Julius Henke <mail@juliushenke.com>
2022-10-24 20:36:46 +0200

    test: add testcontainers to spin up PostgresSQL database

--- backend/src/test/kotlin/ProjectConfig.kt
+++ backend/src/test/kotlin/ProjectConfig.kt
@@ -2,7 +2,28 @@
 
 import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.extensions.spring.SpringExtension
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
 
+
+@Testcontainers
 object ProjectConfig: AbstractProjectConfig() {
     override fun extensions() = listOf(SpringExtension)
+
+    @Container
+    var database: PostgreSQLContainer<*> = PostgreSQLContainer(DockerImageName.parse("postgres:14-alpine"))
+        .withEnv("POSTGRES_USER", "postgres")
+        .withEnv("POSTGRES_PASSWORD", "postgres")
+        .withExposedPorts(5432)
+
+    @DynamicPropertySource
+    fun postgreSQLProperties(registry: DynamicPropertyRegistry) {
+        registry.add("spring.datasource.username", database::getUsername)
+        registry.add("spring.datasource.password", database::getPassword)
+    }
 }
