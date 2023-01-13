a1a8566c31dd20865258713d8428cf2e24aeb6fd
Julius Henke <mail@juliushenke.com>
2023-01-13 02:06:52 +0100

    refactor: geojson utils

--- frontend/src/util/geojson.ts
+++ frontend/src/util/geojson.ts
@@ -4,20 +4,14 @@ import worldGeoData from "../resources/world.geo.json";
  * Returns a color for a country so that no touching countries have the same color
  * @param iso_a2 The ISO-3166-A2 Country code
  */
-export function getMapColor9(iso_a2: string): number {
-    const geoData = worldGeoData
-    const mapColor9 = geoData.features.find(feature => feature.properties.iso_a2 === iso_a2)?.properties.mapcolor9
-    if (mapColor9 !== undefined) return mapColor9
-    return 0
+export function getUniqueCountryColor(iso_a2: string): number {
+    return worldGeoData.features.find(feature => feature.properties.iso_a2 === iso_a2)?.properties.mapcolor9 ?? 0
 }
 
 /**
  * Returns the name of a country with according country code
  * @param iso_a2 The ISO-3166-A2 Country code
  */
-export function getFullName(iso_a2: string): string {
-    const geoData = worldGeoData
-    const name = geoData.features.find(feature => feature.properties.iso_a2 === iso_a2)?.properties.name
-    if (name !== undefined) return name
-    return iso_a2
+export function getFullCountryName(iso_a2: string): string {
+    return worldGeoData.features.find(feature => feature.properties.iso_a2 === iso_a2)?.properties.name ?? iso_a2
 }
