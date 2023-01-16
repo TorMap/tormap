450ad7ec8c9250cd4d752abc34f2cfe9d68c23d2
Julius Henke <mail@juliushenke.com>
2023-01-16 02:58:44 +0100

    fix(backend): find family identifiers

--- backend/src/main/kotlin/org/tormap/database/repository/RelayDetailsRepositoryImpl.kt
+++ backend/src/main/kotlin/org/tormap/database/repository/RelayDetailsRepositoryImpl.kt
@@ -2,14 +2,17 @@ package org.tormap.database.repository
 
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.tormap.adapter.dto.RelayFamilyIdentifiersDto
 import org.tormap.adapter.dto.RelayIdentifiersDto
+import org.tormap.database.entity.FIND_FAMILY_IDENTIFIERS_QUERY
 import javax.transaction.Transactional
+
 
 interface RelayDetailsRepositoryImpl : RelayDetailsRepository {
     @Query(
         "SELECT DISTINCT new org.tormap.database.repository.MonthFamilyMembersCount(month, count(familyId)) " +
-                "FROM RelayDetails GROUP BY month ORDER BY month"
+            "FROM RelayDetails GROUP BY month ORDER BY month"
     )
     fun findDistinctMonthFamilyMemberCount(): List<MonthFamilyMembersCount>
 
@@ -18,18 +21,16 @@ interface RelayDetailsRepositoryImpl : RelayDetailsRepository {
 
     @Query(
         "SELECT new org.tormap.adapter.dto.RelayIdentifiersDto(id, fingerprint, nickname) " +
-                "FROM RelayDetails " +
-                "WHERE id in :ids"
+            "FROM RelayDetails " +
+            "WHERE id in :ids"
     )
     fun findRelayIdentifiers(ids: List<Long>): List<RelayIdentifiersDto>
 
     @Query(
-        "SELECT new org.tormap.adapter.dto.RelayFamilyIdentifiersDto(familyId, count(id), '', '') " +
-                "FROM RelayDetails " +
-                "WHERE familyId in :familyIds " +
-                "GROUP BY familyId"
+        name = FIND_FAMILY_IDENTIFIERS_QUERY,
+        nativeQuery = true,
     )
-    fun findFamilyIdentifiers(familyIds: List<Long>): List<RelayFamilyIdentifiersDto>
+    fun findFamilyIdentifiers(@Param("familyIds") familyIds: List<Long>): List<RelayFamilyIdentifiersDto>
 
     @Transactional
     @Modifying
--- backend/src/test/kotlin/org/tormap/Mock.kt
+++ backend/src/test/kotlin/org/tormap/Mock.kt
@@ -3,20 +3,20 @@ package org.tormap
 import org.tormap.database.entity.RelayDetails
 import java.time.LocalDate
 
-fun relayDetailsMock() = RelayDetails(
+fun relayDetailsMock(mockLetter: Char? = null) = RelayDetails(
     "2022",
     LocalDate.of(2022, 2,4),
-    null,
+    mockLetter?.toString(),
     null,
     "172.104.114.113",
     true,
-    "Hydra1",
+    mockLetter?.toString() ?: "Hydra1",
     1073741824,
     1073741824,
     60223,
     null,
     null,
-    "0B5265EBA4BF8F0DFDA73F68928EC5BF7F88DEF1",
+    mockLetter?.toString()?.repeat(40) ?: "0B5265EBA4BF8F0DFDA73F68928EC5BF7F88DEF1",
     true,
     null,
     null,
