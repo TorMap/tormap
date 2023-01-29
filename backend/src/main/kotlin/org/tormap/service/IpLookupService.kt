ca709aec64cf0e7f9eab79cfc7b52b56c23fe035
Julian Goede <julian.goede@pm.me>
2023-01-29 20:23:21 +0100

    added flyway credentials for flyway clean corrected download link in README.md moved mmdb files to urls
    todo:
     - ktlint styling
     - openapi
     - sql error
     - async annotations
     - caching
     - I think we should ship the mmdb files not within the Spring app but rather as external config like a volume or ConfigMap mount in Docker / Kubernetes or even letting it download on it's own
     - model changes:
      - RelayDetails:
        -month is redundant & fingerprint and month could be primary key in database
        - day should be called date and database type should be date
      - RelayLocation:
        - fingerprint and day should be primary key
        - day should be database type date

--- backend/src/main/kotlin/org/tormap/service/IpLookupService.kt
+++ backend/src/main/kotlin/org/tormap/service/IpLookupService.kt
@@ -8,11 +8,17 @@ import mu.KotlinLogging
 import org.springframework.core.io.Resource
 import org.springframework.stereotype.Service
 import org.tormap.config.value.IpLookupConfig
+import java.io.File
+import java.io.InputStream
 import java.math.BigDecimal
 import java.math.RoundingMode
 import java.net.InetAddress
+import java.nio.file.Files
+import java.nio.file.Path
 import java.util.zip.GZIPInputStream
+import kotlin.io.path.exists
 import com.maxmind.geoip2.record.Location as GeoIp2Location
+
 
 /**
  * This service handles location tasks requiring a geolocation database
@@ -55,15 +61,38 @@ class IpLookupService(ipLookupConfig: IpLookupConfig) {
     }
 
     /**
+     * Checks if the provided Resource exists in the tmp folder. If not, it is stored. It is stored by its name, if the
+     * resource changes, but not it's name, this change is not picked up and the old version is used.
+     * Either the backing file has to be removed, a new name has to be used or the OS has to trigger a tmp file clean
+     * up.
+     */
+    private fun storeOrLoad(resource: Resource): File {
+        // input stream is expensive, so make it lazy and only evaluate if we need it
+        fun store(filename: String, inputStream: () -> InputStream): File {
+            val tmpResourcePath = Path.of(System.getProperty("java.io.tmpdir"), filename)
+
+            if (!tmpResourcePath.exists()) {
+                Files.copy(inputStream(), tmpResourcePath)
+            }
+
+            return tmpResourcePath.toFile()
+        }
+
+        val filename = resource.filename!!
+        return when {
+            resource.isFile -> resource.file
+            filename.endsWith(".gz") -> store(filename.removeSuffix(".gz")) { GZIPInputStream(resource.inputStream) }
+            else -> store(filename, resource::getInputStream)
+        }
+    }
+
+    /**
      * Create a database reader for the [.mmdb file format](https://maxmind.github.io/MaxMind-DB/)
      */
     private fun maxmindTypeDatabaseReader(databaseFile: Resource, shouldCache: Boolean): DatabaseReader {
-        val inputStream = when {
-            databaseFile.filename?.endsWith(".gz") == true -> GZIPInputStream(databaseFile.inputStream)
-            else -> databaseFile.inputStream
-        }
+        val file = storeOrLoad(databaseFile)
 
-        val reader = DatabaseReader.Builder(inputStream)
+        val reader = DatabaseReader.Builder(file)
         return if (shouldCache) reader.withCache(CHMCache()).build() else reader.build()
     }
 }
