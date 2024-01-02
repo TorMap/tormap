863af11a1933099ae216b45793959cda47ae2e91
Julius Henke <mail@juliushenke.com>
2024-01-02 19:27:51 +0100

    chore: remove all shell installation and run scripts
    BREAKING-CHANGE: The shell scripts are rarely useful for a typical developer when installing requirements and running the project. Often it is only necessary to work on either frontend or backend. To keep documentation and installation lean, the scripts are now removed entirely.

--- run.bat
+++
@@ -1,5 +0,0 @@
-echo Please wait while backend and frontend servers are starting
-
-cd scripts
-start frontend.bat
-backend.bat
--- scripts/backend.bat
+++
@@ -1,2 +0,0 @@
-cd ../backend || exit
-./gradle.bat && ./gradle.bat bootRun
